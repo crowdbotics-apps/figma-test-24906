fab50701257c23ea68f49f34c4faeecd0bf186ef
Crowdbotics <team@crowdbotics.com>
2021-03-08 05:01:51 +0000

    Update models

--- backend/figma_test_24906/wsgi.py
+++ backend/figma_test_24906/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'figma_test_24906.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "figma_test_24906.settings")
 
 application = get_wsgi_application()
